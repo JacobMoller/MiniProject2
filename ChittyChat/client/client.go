a7c73c35e55a09ca41859006da9c0e57b03a6091
JacobM <jacj@itu.dk>
2021-10-12 16:17:31 +0200

    Basic server/client

--- ChittyChat/client/client.go
+++ ChittyChat/client/client.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"bufio"
+	"context"
+	"log"
+	"os"
+	"strings"
+
+	"Miniproject2/ChittyChat/protobuf"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	log.Print("Welcome to ChittyChat. Please enter a username:")
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	name := strings.Replace(text, "\n", "", 1)
+
+	conn, err := grpc.Dial(":8080", grpc.WithInsecure(), grpc.WithBlock()) //maybe it has to be: localhost:8080
+	if err != nil {                                                        //error can not establish connection
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := protobuf.NewChittyChatClient(conn)
+
+	response, err := client.Publish(context.Background(), &protobuf.PublishRequest{Time: "nu", Type: "JOIN", Message: "JOIN MSG", From: name})
+	if err != nil {
+		log.Fatalf("could not get chat list: %v", err)
+	}
+
+	response2, err2 := client.Publish(context.Background(), &protobuf.PublishRequest{Time: "nu", Type: "CHAT", Message: "Japan!", From: name})
+	if err2 != nil {
+		log.Fatalf("could not get chat list: %v", err)
+	}
+
+	log.Print(response.GetMessage())
+	log.Print(response2.GetMessage())
+}
--- ChittyChat/server/server.go
+++ ChittyChat/server/server.go
@@ -1,1 +1,67 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"Miniproject2/ChittyChat/protobuf"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	protobuf.UnimplementedChittyChatServer
+}
+
+//the type of data we can handle
+type Message struct {
+	Name string `json:"name"`
+}
+
+func (s *server) Publish(ctx context.Context, in *protobuf.PublishRequest) (*protobuf.PublishReply, error) {
+	if in.Type == "JOIN" {
+		return &protobuf.PublishReply{Message: "Participant " + in.From + " joined Chitty-Chat at Lamport time L"}, nil
+	} else {
+		return &protobuf.PublishReply{Message: in.From + ": " + in.Message}, nil
+	}
+	//implement actual logic of getting the courses
+	//.HelloReply{Message: fmt.Sprint(courses[num].Name) + ";" + fmt.Sprint(courses[num].StudentSatisfactionRating)}, nil
+}
+
+/*func (s *server) Broadcast(ctx context.Context) (*protobuf.GetCourseListReply, error) {
+	//implement actual logic of getting the courses
+
+	var listOfChatMessages = []*protobuf.Course{
+		{From: "Jacob", Type: "message", Content: "Hej verden!"},
+		{From: "Jeppe", Type: "message", Content: "Yo!"},
+		{From: "Freja", Type: "message", Content: "Japan!"},
+	}
+	return &protobuf.broadcastReply{listOfChatMessages}, nil //.HelloReply{Message: fmt.Sprint(courses[num].Name) + ";" + fmt.Sprint(courses[num].StudentSatisfactionRating)}, nil
+}*/
+
+func main() {
+	//port :8080
+	lis, err := net.Listen("tcp", ":8080")
+
+	if err != nil { //error before listening
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer() //we create a new server
+	protobuf.RegisterChittyChatServer(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil { //error while listening
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+/*
+
+	var listOfChatMessages = []*protobuf.Course{
+		{From: "Jacob",Type:"message",Content:"Hej verden!"},
+		{From: "Jeppe",Type:"message",Content:"Yo!"},
+		{From: "Freja",Type:"message",Content:"Japan!"},
+	}
+
+*/
